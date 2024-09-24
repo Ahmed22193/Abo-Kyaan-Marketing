c9213512690625dbac65786cbd4cab43c655169d
Ahmed22193 <Ahmed221050193@gmail.com>
2024-09-24 13:01:44 +0300

    Done some styles

--- marketing.js
+++ marketing.js
@@ -1,4 +1,24 @@
 let up = document.querySelector("#up");
+
+let inputname = document.querySelector('#inputname').value;
+let inputemail = document.querySelector('#inputemail').value;
+let inputtext = document.querySelector('#textarea').value;
+let SUB = document.querySelector('#SUB');
+
+
+let Submit = function(){
+    var WhatsAppURL = "https://wa.me/201210382651?text=" +
+    encodeURIComponent(
+    " From WebSite " + "\n" +
+    "اسم العميل : " + inputname + "\n" +
+    " الايميل : " + inputemail + "\n" +
+    "استفسار : " + inputtext + "\n"
+    );
+    window.open(WhatsAppURL, "_blank").focus();
+}
+
+
+
 up.style.display = "none";
 window.onscroll = function() {
     scrollFunction();
@@ -11,25 +31,3 @@ function scrollFunction() {
     }
 }
 
-
-
-
-/* 
-
-let LINKS = document.getElementById('LINKS');
-let temp = 'block';
-function LINKSS(){
-    if(temp==='block'){
-        LINKS.style.display='block';
-        temp = 'none';
-    }
-    else if(temp ==='none'){
-        LINKS.style.display='none';
-        temp = 'block';
-    }
-}
-function LIN(){
-    LINKS.style.display='none';
-    temp = 'block';
-}
- */
